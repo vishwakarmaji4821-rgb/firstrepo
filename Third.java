6a4985c11c75ebebf10b035ac949127e5fa27816
vishwakarmaji4821-rgb <vishwakarmaji4821@gmail.com>
2026-03-09 10:41:00 +0530

    Other files added in the repo

---
+++ Third.java
@@ -0,0 +1,10 @@
+public class Third {
+    public static void main(String[] args) {
+        int  a =10, b =20,c;
+        c = b +a;
+        b = c-b;
+        a =a+b;
+        System.out.println(" a :"+a+" b :"+b);
+    }
+    
+}
